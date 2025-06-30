d4637a990d25513270a4bfbf866644272369d0f9
Shubham Kumar <shubhamkumar821807@gmail.com>
2025-07-01 01:37:47 +0530

    Update PaymentPage.js

--- components/PaymentPage.js
+++ components/PaymentPage.js
@@ -1,5 +1,5 @@
 "use client"
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import React from 'react'
 import Script from 'next/script'
 import { fetchuser, initiate, fetchpayments } from '@/actions/useractions'
